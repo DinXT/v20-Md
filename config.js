10936bab74875265c316d210d48c15f6cfa68b10
Coding Student <92778687+DinXT@users.noreply.github.com>
2022-03-01 16:13:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6288279268363'] // Put your number here
+global.owner = ['6281380674817'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -20,40 +20,40 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'https://pastelink.net/v20Md',
+  'https://api.xteam.xyz': '4466db87838bb741',
   'https://api.lolhuman.xyz': 'rey2k21',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.me': 'apivinz',
   'https://pencarikode.xyz': 'pais',
-  'https://apikey-bear3.herokuapp.com': 'https://pastelink.net/v20Md',
+  'https://apikey-bear3.herokuapp.com': 'dtqFoxzG',
   'https://viko-api.herokuapp.com': 'vinko',
   'https://leyscoders-api.herokuapp.com': 'dappakntlll'
 }
 
 lolkey = 'rey2k21'
 zekskey = 'apivinz'
-xteamkey = 'https://pastelink.net/v20Md'
+xteamkey = '4466db87838bb741'
 leyskey = 'dappakntlll'
-bearkey = 'https://pastelink.net/v20Md'
-ApiiKey = 'https://pastelink.net/v20Md'
+bearkey = 'dtqFoxzG'
+ApiiKey = 'dtqFoxzG'
 vinkey = 'vinko'
 
-namabot = 'Yuki Bot'
-namalu = 'King Of Bear'
-footer = '\n©King Of Bear'
+namabot = '<ᴹᴿ᭄DinXTོ ×፝֟͜× `bot/>'
+namalu = 'DinXT'
+footer = '\n© ᴹᴿ᭄DinXTོ ×፝֟͜× `bot'
 kasihcaption = `Nih Sayang`
 
 // Sticker WM
 global.stiker_wait = '_*Tunggu Sebentar...*_'
-global.packname = 'YukiBot Multi device'
-global.author = 'King Of Bear'
-global.email = 'drakipul1016@gmail.com'
+global.packname = 'DinXT'
+global.author = 'YukiBot-md'
+global.email = 'itsdinxt@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '©King Of Bear Bot-Md'
+global.wm = '©DinXT'
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
-global.wait = '_*Tunggu Sebentar Sedang Di Proses...*_'
-global.eror = '_*Server Error*_'
+global.wait = 'Proses Banh :v'
+global.eror = 'emror :v'
 
 global.multiplier = 69 // The higher, The harder levelup
 
